2bb9e13de962cc1b7db8613069ff8ff0a2ee72a2
dorigodev <murillodorigo@gmail.com>
2025-05-12 10:43:56 -0300

    Feat: add repository annotation

--- src/main/java/dev/dorigo/springcommerce/api/repository/CategoryRepository.java
+++ src/main/java/dev/dorigo/springcommerce/api/repository/CategoryRepository.java
@@ -2,6 +2,8 @@ package dev.dorigo.springcommerce.api.repository;
 
 import dev.dorigo.springcommerce.api.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
