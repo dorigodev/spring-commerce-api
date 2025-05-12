874a52b98ea37cd94648f0450c6d040db3f1c645
dorigodev <murillodorigo@gmail.com>
2025-05-12 11:15:23 -0300

    Feat: add endpoints to category

---
+++ src/main/java/dev/dorigo/springcommerce/api/controller/CategoryController.java
@@ -0,0 +1,59 @@
+package dev.dorigo.springcommerce.api.controller;
+
+import dev.dorigo.springcommerce.api.controller.request.CategoryRequest;
+import dev.dorigo.springcommerce.api.controller.response.CategoryResponse;
+import dev.dorigo.springcommerce.api.domain.Category;
+import dev.dorigo.springcommerce.api.mapper.CategoryMapper;
+import dev.dorigo.springcommerce.api.service.CategoryService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/category")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService service;
+
+    @PostMapping
+    public ResponseEntity<CategoryResponse> create(@RequestBody @Valid CategoryRequest request) {
+        var category = service.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(CategoryMapper.toResponse(category));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryResponse> get(@PathVariable Long id) {
+        return ResponseEntity.ok(CategoryMapper.toResponse(service.getyById(id)));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponse>> getAll() {
+        return ResponseEntity.ok(service.getAll()
+                .stream()
+                .map(CategoryMapper::toResponse)
+                .toList());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryResponse> update(@PathVariable Long id, @RequestBody @Valid CategoryRequest request) {
+        var category = service.update(id, request);
+        return ResponseEntity.ok(CategoryMapper.toResponse(category));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Optional<Category> category = Optional.ofNullable(service.getyById(id));
+        if (category.isPresent()) {
+            service.delete(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+}
