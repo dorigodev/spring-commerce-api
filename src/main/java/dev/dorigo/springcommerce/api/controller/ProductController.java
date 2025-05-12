fbe528503eb3055c0ba51c4a4dc620087b2bca2c
dorigodev <murillodorigo@gmail.com>
2025-05-12 13:54:58 -0300

    feat: create endpoint and service for find and update Product

--- src/main/java/dev/dorigo/springcommerce/api/controller/ProductController.java
+++ src/main/java/dev/dorigo/springcommerce/api/controller/ProductController.java
@@ -9,22 +9,40 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
 @RequiredArgsConstructor
 public class ProductController {
 
-    private final ProductService productService;
+    private final ProductService service;
 
     @PostMapping
     public ResponseEntity<ProductResponse> createProduct(@RequestBody @Valid ProductRequest request) {
-        Product product = productService.save(request);
+        Product product = service.save(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(ProductMapper.toResponse(product));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getAllProducts() {
+        return ResponseEntity.ok(service.findAll()
+                .stream()
+                .map(ProductMapper::toResponse)
+                .toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable Long id) {
+        return ResponseEntity.ok(ProductMapper.toResponse(service.findById(id)));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponse> updateProduct(@PathVariable Long id, @RequestBody @Valid ProductRequest request) {
+        Product product = service.update(id, request);
+        return ResponseEntity.ok(ProductMapper.toResponse(product));
     }
 
 }
--- src/main/java/dev/dorigo/springcommerce/api/service/ProductService.java
+++ src/main/java/dev/dorigo/springcommerce/api/service/ProductService.java
@@ -6,7 +6,9 @@ import dev.dorigo.springcommerce.api.domain.Product;
 import dev.dorigo.springcommerce.api.mapper.ProductMapper;
 import dev.dorigo.springcommerce.api.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +23,27 @@ public class ProductService {
     public Product save(ProductRequest request) {
         Product product = ProductMapper.toProduct(request);
         product.setCategory(this.findCategories(product.getCategory()));
+        return repository.save(product);
+    }
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    public Product findById(Long id) {
+        return repository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    public Product update(Long id, ProductRequest request) {
+        var product = repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        var update = ProductMapper.toProduct(request);
+        product.setName(update.getName());
+        product.setDescription(update.getDescription());
+        product.setPrice(update.getPrice());
+        product.setImageUrl(update.getImageUrl());
+        product.setStockQuantity(update.getStockQuantity());
+        product.getCategory().clear();
+        product.setCategory(this.findCategories(update.getCategory()));
         return repository.save(product);
     }
 
